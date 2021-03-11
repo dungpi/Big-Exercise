6c1b5283424fa98111467f9194010f6a541ce0c3
Dung <72554580+dungpi@users.noreply.github.com>
2021-03-11 18:19:13 +0700

    Update Quanlisach.cpp

--- Quanlisach.cpp
+++ Quanlisach.cpp
@@ -9,15 +9,18 @@ struct Tacgia{
     char matacgia[30];
     char tentacgia[30];
     DateTime *ngaysinh;   
+    
 };
 struct Nhaxuatban{
     char manhaxuatban [30];
     char tennhaxuatban [30];
-    DateTime *namxuatbanx;
+    DateTime *namxuatban;
 };
 struct Sach{
     char masach [30];
     char ten[30];
+    char theloai[50];
+    int sotuasach;
     Tacgia *tacgia;
     Nhaxuatban *nhaxuatban;
     int soluong;
@@ -30,28 +33,23 @@ struct Nguoimuon{
 void enter (DateTime *time );
 void enter (Nhaxuatban *nhaxuatban);
 void enter (Tacgia *tacgia);
+void enter (Sach *sach);
+void enter (Sach *danhsach, int &sotuasach);
 void print (DateTime *time);
 void print (Nhaxuatban *nhaxuatban);
+void print (Tacgia *tacgia);
 bool validDay(DateTime *time);
 bool leapYear(DateTime *time);
 int main (){
-    DateTime *time;
-    Nhaxuatban *nhaxuatban;
-    Tacgia *tacgia;
-
-    time = (DateTime *)malloc(sizeof(DateTime));
-    nhaxuatban = (Nhaxuatban *)malloc(sizeof(Nhaxuatban));
-    tacgia= (Tacgia*)malloc(sizeof(Tacgia));
-
-    enter(nhaxuatban);
-    print(nhaxuatban);
-    enter(time);
-    print(time);
+    Sach *sach;
+    int sotuasach;
+    sach = (Sach *)malloc(sizeof(Sach));
+    enter (sach,sotuasach);
     return 0;
 }
 void enter (DateTime *time ){
     do{
-    printf ("\nNhap Ngay: ");
+    printf ("Nhap Ngay: ");
     scanf ("%d",&time -> day);
     printf ("Nhap Thang: ");
     scanf ("%d",&time -> month);
@@ -98,21 +96,63 @@ bool leapYear(DateTime *time){
     return time->year % 4 == 0;
 }
 void print (DateTime *time){
-    printf("Ngay Thang Nam xuat ban:%.2d/%.2d/%.4d",time->day,time->month,time->year);
+    printf("%.2d/%.2d/%.4d",time->day,time->month,time->year);
 }
 void enter(Nhaxuatban *nhaxuatban){
+    fflush(stdin);
     printf("Nhap ma nha xuat ban:");
     gets(nhaxuatban->manhaxuatban);
     printf("Nhap ten nha xuat ban:");
     gets(nhaxuatban->tennhaxuatban);
+    nhaxuatban->namxuatban = (DateTime*)(malloc(sizeof(DateTime)));
+    enter(nhaxuatban->namxuatban);
 }
 void print(Nhaxuatban *nhaxuatban){
-    printf("Thong tin nha xuat ban: %s %s",nhaxuatban->manhaxuatban,nhaxuatban->tennhaxuatban);
+    printf("Thong tin nha xuat ban: %s %s\n",nhaxuatban->manhaxuatban,nhaxuatban->tennhaxuatban);
+    printf("Xuat Ban Nam: ");
+    print (nhaxuatban->namxuatban);
 }
 void enter (Tacgia *tacgia){
-    printf("\nNhap Ten Tac Gia: ");
+    fflush(stdin);
+    printf("Nhap Ten Tac Gia: ");
     gets (tacgia->tentacgia);
     printf("Nhap Ma Tac Gia: ");
     gets (tacgia->matacgia);
+    tacgia->ngaysinh = (DateTime*)(malloc(sizeof(DateTime))); 
     enter (tacgia->ngaysinh);
+}
+void print (Tacgia *tacgia){
+    printf ("\n-----------Tac Gia-------------");
+    printf ("\nTac Gia: %s",tacgia->tentacgia);
+    printf ("\nMa Tac Gia: %s",tacgia->matacgia);
+    printf ("\nNam Sinh Tac Gia: ");
+    print(tacgia->ngaysinh);
+    printf ("\n-------------------------------");
+}
+void enter (Sach *sach){
+    fflush(stdin);
+    printf ("Nhap Ma Sach: ");
+    gets(sach->masach);
+    printf ("Nhap Ten Sach: ");
+    gets(sach->ten);
+    printf ("Nhap The Loai Sach: ");
+    gets(sach->theloai);
+    sach->tacgia = (Tacgia*)(malloc(sizeof(Tacgia)));
+    enter(sach->tacgia);
+    sach->nhaxuatban = (Nhaxuatban*)(malloc(sizeof(Nhaxuatban)));
+    enter(sach->nhaxuatban);
+    printf ("Nhap So Luong Sach: ");
+    scanf("%d",&sach->soluong);
+    printf ("Nhap Gia Sach: ");
+    scanf("%f",&sach->gia);
+}
+void enter (Sach *sach, int &sotuasach){
+    printf ("Nhap So Luong Sach Trong Thu Vien: ");
+    scanf("%d",&sach->sotuasach);
+    sach = (Sach*)realloc(sach,(sotuasach)*sizeof(Sach));
+    for (int i = 0; i < sotuasach; i++){
+        printf ("\n\n--------------------------------");
+        printf ("\nNhap thong tin Sach %d\n",i+1);
+        enter(&*(sach+1));
+    }
 }
