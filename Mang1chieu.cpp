46119323fbb2b0cf3d0ef9dba0d1acd8dadf0ea3
Dung <72554580+dungpi@users.noreply.github.com>
2021-03-16 00:05:52 +0700

    Update Mang1chieu.cpp

--- Mang1chieu.cpp
+++ Mang1chieu.cpp
@@ -2,12 +2,15 @@
 // -    Nhập xuất mảng 1 chiều
 // -    Tìm giá trị max trong mảng một chiều
 // -    Tìm vị trí max trong mảng một chiều
+// -    tìm top n cac phan tu, với n nhap từ bàn phím
 #include <stdio.h>
 #include <stdlib.h>
 #define MAX 50
 void enter (int *array,int &n);
 void positionMax (int *array,int n);
 void print (int *array, int n);
+void sapXepGiam (int *array,int n);
+void printtop (int *array,int n);
 int main (){
     int *array,n;
     array = (int *) malloc(sizeof(int));
@@ -21,8 +24,10 @@ int main (){
     }while(n <= 0 || n > MAX);
     enter(array,n);
     print(array,n);
-    printf("\n=========MAX============");
+    printf("\n===========MAX===========");
     positionMax(array,n);
+    sapXepGiam(array,n);
+    printtop (array,n);
     return 0;
 }
 void enter (int *array,int &n){
@@ -47,5 +52,23 @@ void positionMax (int *array,int n){
             pos = i;
         }
     }
-    printf("\nGia tri Max: %d,Vi tri:%d",max,pos);
+    printf("\nGia tri Max: %d,Vi tri:%d\n",max,pos);
+}
+void sapXepGiam (int *array,int n){
+	int temp;
+	for (int i = 0; i < n; i++){
+		for(int j = i + 1; j < n; j++){
+			if (*(array+i) < *(array+j)){
+				temp = *(array+i);
+				*(array+i) = *(array+j);
+				*(array+j) = temp;
+			}
+		}
+	}	
+}
+void printtop (int *array,int n){
+    printf("\n===========TOP===========");
+    for (int i = 0;i < n;i++){
+         printf("\nTop %d = %d\t",i+1,*(array+i));
+    }
 }
